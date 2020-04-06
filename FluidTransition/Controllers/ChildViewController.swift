a6069fb8a25dc667f3d6a47ae0400eb3a051580d
MeGaPk <892182+MeGaPk@users.noreply.github.com>
2020-04-06 11:34:38 +0300

    test

--- FluidTransition/Controllers/ChildViewController.swift
+++ FluidTransition/Controllers/ChildViewController.swift
@@ -7,34 +7,45 @@
 //
 
 import UIKit
+import MessageUI
 
-class ChildViewController: UIViewController {
-    
+class ChildViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view.backgroundColor = UIColor(hue: 0.2, saturation: 0.8, brightness: 0.9, alpha: 1)
         view.layer.cornerRadius = 24
-        
-        addDismissButton()
+
+        addShareButton()
     }
-    
-    private func addDismissButton() {
+
+    private func addShareButton() {
         let button = UIButton(type: .system)
-        button.setTitle("Dismiss", for: .normal)
+        button.setTitle("SMS", for: .normal)
         button.addTarget(self, action: #selector(dismissSelf), for: .touchUpInside)
         view.addSubview(button)
-        
+
         button.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             button.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             button.widthAnchor.constraint(equalToConstant: 200),
             button.heightAnchor.constraint(equalToConstant: 80),
-            ])
+        ])
     }
-    
+
     @objc func dismissSelf() {
-        self.dismiss(animated: true)
+        let composeVC = MFMessageComposeViewController()
+        composeVC.messageComposeDelegate = self
+        composeVC.recipients = ["4085551212"]
+        composeVC.body = "Hello from California!"
+        self.present(composeVC, animated: true, completion: nil)
+
+    }
+
+    func messageComposeViewController(_ controller: MFMessageComposeViewController,
+                                      didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
     }
 }
